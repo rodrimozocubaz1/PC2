b57f56f11e06dbc98e1595771e46a076748a250a
Rodrigo Mozo <rodrigo.mozo.01@gmail.com>
2018-11-20 08:38:28 -0500

    Ultimate version

--- Pregunta03.py
+++ Pregunta03.py
@@ -4,9 +4,9 @@ matriz = []
 for i in range(f):
     matriz.append([1]*c)
 
-n= matriz[1][2]
-m= matriz[2][2]
-z= matriz[1][2]
+n= matriz[0][2]
+m= matriz[1][2]
+z= matriz[2][2]
 
 suma_diagonal= n+m+z
 
